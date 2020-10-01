f01ed2edc65c44069a553c7a9469b062e53c393e
Alin Jerpelea <alin.jerpelea@sony.com>
2020-10-01 20:12:09 +0200

    Revert "Only disable wifi if WPA_COUNTRY is not set (#416)"
    This reverts commit be8daaa7210d8a8df77792e7bcb3ec509d8e995e.

--- stage2/02-net-tweaks/01-run.sh
+++ stage2/02-net-tweaks/01-run.sh
@@ -25,12 +25,7 @@ network={
 EOL
 fi
 
-# Disable wifi on 5GHz models if WPA_COUNTRY is not set
+# Disable wifi on 5GHz models
 mkdir -p "${ROOTFS_DIR}/var/lib/systemd/rfkill/"
-if [ -n "$WPA_COUNTRY" ]; then
-    echo 0 > "${ROOTFS_DIR}/var/lib/systemd/rfkill/platform-3f300000.mmcnr:wlan"
-    echo 0 > "${ROOTFS_DIR}/var/lib/systemd/rfkill/platform-fe300000.mmcnr:wlan"
-else
-    echo 1 > "${ROOTFS_DIR}/var/lib/systemd/rfkill/platform-3f300000.mmcnr:wlan"
-    echo 1 > "${ROOTFS_DIR}/var/lib/systemd/rfkill/platform-fe300000.mmcnr:wlan"
-fi
+echo 1 > "${ROOTFS_DIR}/var/lib/systemd/rfkill/platform-3f300000.mmcnr:wlan"
+echo 1 > "${ROOTFS_DIR}/var/lib/systemd/rfkill/platform-fe300000.mmcnr:wlan"
